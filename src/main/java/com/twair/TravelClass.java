1dd4b8212f840f09d3367e46cd924af6e8f6db2a
Srinath Avancha <Srinath Avancha>
2016-04-03 11:57:56 +0530

    Adding project

---
+++ src/main/java/com/twair/TravelClass.java
@@ -0,0 +1,38 @@
+package com.twair;
+
+public class TravelClass {
+    private ClassType classType;
+    private Integer totalSeats;
+    private Integer occupiedSeats;
+    private Double basePrice;
+
+    public TravelClass(ClassType classType, Integer totalSeats, Double basePrice) {
+        this.classType = classType;
+        this.totalSeats = totalSeats;
+        this.occupiedSeats = 0;
+        this.basePrice = basePrice;
+    }
+
+    public void book(int numberOfSeats) throws Exception {
+        if(canBook(numberOfSeats) == false) {
+            throw new Exception("Booking can not be made");
+        }
+        occupiedSeats += numberOfSeats;
+    }
+
+    public boolean canBook(Integer numberOfSetas) {
+        if(occupiedSeats + numberOfSetas > totalSeats) {
+            return false;
+        }
+        return true;
+    }
+
+    public ClassType getClassType() {
+        return classType;
+    }
+
+    public Double getBasePrice() {
+        return basePrice;
+    }
+
+}
